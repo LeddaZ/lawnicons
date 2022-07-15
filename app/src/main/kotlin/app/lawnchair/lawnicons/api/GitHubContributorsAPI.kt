abbe39c78c32e7446b69f036cc9a6198f7d898e0
LeddaZ <leonardoledda@gmail.com>
2022-07-15 17:32:19 +0200

    Change repo link to mine

--- app/src/main/kotlin/app/lawnchair/lawnicons/api/GitHubContributorsAPI.kt
+++ app/src/main/kotlin/app/lawnchair/lawnicons/api/GitHubContributorsAPI.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface GitHubContributorsAPI {
 
-    @GET("repos/LawnchairLauncher/lawnicons/contributors")
+    @GET("repos/LeddaZ/lawnicons/contributors")
     suspend fun getContributors(): List<GitHubContributor>
 }
